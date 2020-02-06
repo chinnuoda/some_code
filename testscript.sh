29bf9ced098d52fe6ecd6d0e6d5fdd11aa7424ad
chinnuoda <59340891+chinnuoda@users.noreply.github.com>
2020-02-06 15:47:02 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,8 +3,3 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "this was run automagically"
-<<<<<<< HEAD
-echo "hii priya"
-=======
-echo "hii"
->>>>>>> cdf35b5db05b852988a12475cc40d7f39c02f5ec
